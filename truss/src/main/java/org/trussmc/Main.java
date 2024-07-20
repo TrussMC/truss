70eafdb142d845be9b5375a7f04212d0c967e8b8
Switch672 <151470782+Switch672@users.noreply.github.com>
2024-07-20 14:50:27 -0600

    Initial commit

---
+++ truss/src/main/java/org/trussmc/Main.java
@@ -0,0 +1,12 @@
+package org.trussmc;
+
+import org.geysermc.mcprotocollib.
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServerListener server = new ServerListener() {
+            public static void connected()
+        };
+    }
+}
